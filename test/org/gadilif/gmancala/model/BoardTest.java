621e4a094007017cf68d000f6b0771b34a782f6c
gadilif <gadilif@11be5e6a-4b2c-0410-b02e-8710c73196a8>
2007-03-24 17:51:14 +0000

    Added basic board logic and manual play

--- test/org/gadilif/gmancala/model/BoardTest.java
+++ test/org/gadilif/gmancala/model/BoardTest.java
@@ -1,17 +1,57 @@
 package org.gadilif.gmancala.model;
 
 import org.gadilif.gmancala.model.Board;
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class BoardTest {
+	
+	private Board board;
+	
+	@Before
+	public void init() {
+		board = new Board();
+		board.init();
+	}
+	
 	@Test
 	public void boardInit() {
-		Board board = new Board();
-		assertFalse("board should not be initialized at that time", board.isInitialized());
-		board.init();
 		assertTrue("board should be initialized now", board.isInitialized());
-		
+	}
+	
+	@Test
+	public void getCellValue() {
+		assertEquals(4, board.getCellValue(1));
+		System.out.println(board);
+	}
+	
+	@Test
+	public void getCellAfterPlay() {
+		board.play(4);
+		assertEquals(0, board.getCellValue(4));
+		assertEquals(1, board.getRightHoleValue());
+		System.out.println(board);
+	}
+	
+	@Test
+	public void getCellAfterPlay2() {
+		board.play(11);
+		assertEquals(0, board.getCellValue(11));
+		assertEquals(1, board.getLeftHoleValue());
+		System.out.println(board);
+	}
+	
+	@Test
+	public void consecutivePlays() {
+		board.play(4);
+		assertEquals(1, board.getRightHoleValue());
+		assertFalse(board.canPlay(4));
+		board.play(4);
+		assertEquals(1, board.getRightHoleValue());
+		board.play(5);
+		assertEquals(2, board.getRightHoleValue());
+		System.out.println(board);
 	}
 
 }
