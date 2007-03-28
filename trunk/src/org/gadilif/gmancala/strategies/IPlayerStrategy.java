4a0322988d9880e73975b61940636e9c3b9f92b4
gadilif <gadilif@11be5e6a-4b2c-0410-b02e-8710c73196a8>
2007-03-28 14:25:54 +0000

    added RandomPlayer

--- trunk/src/org/gadilif/gmancala/strategies/IPlayerStrategy.java
+++ trunk/src/org/gadilif/gmancala/strategies/IPlayerStrategy.java
@@ -1,6 +1,5 @@
 package org.gadilif.gmancala.strategies;
 
-import org.gadilif.gmancala.controller.BoardController.PlayResult;
 import org.gadilif.gmancala.model.PlayerType;
 
 public interface IPlayerStrategy {
